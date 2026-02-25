d01ced972f13540c69773f4f386243b1c1018f05
Rivera-CuentaOficial <146663110+Rivera-CuentaOficial@users.noreply.github.com>
2026-02-25 05:33:28 -0300

    refactor(JobApplicationOptions): remove IncludeReview property from options

--- src/Domain/Options/JobApplicationOptions.cs
+++ src/Domain/Options/JobApplicationOptions.cs
@@ -20,10 +20,5 @@ namespace backend.src.Domain.Options
         /// Incluir información de la oferta laboral a la que se postuló en la consulta
         /// </summary>
         public bool IncludeJobOffer { get; set; } = false;
-
-        /// <summary>
-        /// Incluir información de la revisión asociada a la postulación en la consulta, si es que llegara a existir
-        /// </summary>
-        public bool IncludeReview { get; set; } = false;
     }
 }
